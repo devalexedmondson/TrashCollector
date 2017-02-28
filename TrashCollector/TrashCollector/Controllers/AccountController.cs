b6cb0b52563ed5105b900a26775dacfa1e1aa2b8
devalexedmondson <alexedmondson20@gmail.com>
2017-02-28 17:59:12 -0600

    seperated actions, one for user and one for collector, linked all register links to user register for now

--- TrashCollector/TrashCollector/Controllers/AccountController.cs
+++ TrashCollector/TrashCollector/Controllers/AccountController.cs
@@ -135,19 +135,19 @@ namespace TrashCollector.Controllers
         }
 
         //
-        // GET: /Account/Register
+        // GET: /Account/UserRegister
         [AllowAnonymous]
-        public ActionResult Register()
+        public ActionResult UserRegister()
         {
             return View();
         }
 
         //
-        // POST: /Account/Register
+        // POST: /Account/UserRegister
         [HttpPost]
         [AllowAnonymous]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Register(RegisterViewModel model)
+        public async Task<ActionResult> UserRegister(RegisterViewModel model)
         {
             if (ModelState.IsValid)
             {
@@ -171,6 +171,45 @@ namespace TrashCollector.Controllers
             // If we got this far, something failed, redisplay form
             return View(model);
         }
+
+        //
+        // GET: /Account/CollectorRegister
+        [AllowAnonymous]
+        public ActionResult CollectorRegister()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Account/CollectorRegister
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> CollectorRegister(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+                var result = await UserManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
+
+                    // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
+                    // Send an email with this link
+                    // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+                    // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+                    // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+                    return RedirectToAction("Index", "Home");
+                }
+                AddErrors(result);
+            }
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+
 
         //
         // GET: /Account/ConfirmEmail
